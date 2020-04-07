5ebe46e87aca06cfa1e4592ff0b8281bf123cb88
Pablo <pablocolchiesqui@gmail.com>
2020-04-07 22:21:15 +0200

    finished exercises for section 1.2

--- CppPrimer/CppPrimer.cpp
+++ CppPrimer/CppPrimer.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+int printHelloWorld();
+int multiply(int v1, int v2);
+
 int main()
 {
     std::cout << "Enter 2 numbers:" << std::endl;
@@ -8,5 +11,24 @@ int main()
     std::cout << "The sum of " << v1 << " and " << v2
         << " is " << v1 + v2 << std::endl;
 
+    printHelloWorld();
+    multiply(v1, v2);
+
     return 0;
+}
+
+int printHelloWorld() 
+{
+        std::cout << "Hello World" << std::endl;
+
+        return 0;
+}
+
+int multiply(int v1, int v2) 
+{
+    int result = v1 * v2;
+    std::cout << "The multiplication of " << v1 << " and " << v2
+        << " is " << result << std::endl;
+
+    return result;
 }
---
+++ CppPrimer/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int helloWorld() {
+	std::cout << "Hello World";
+
+	return 0;
+}
